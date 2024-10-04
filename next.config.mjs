3a0e3da4bbfa2e48863e3494a13afe28645b6ded
Maryam Nadeem <162018235+maryumxx@users.noreply.github.com>
2024-10-04 09:02:20 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// next.config.js
+module.exports = {
+  // Enable the `src` directory for Next.js pages
+  experimental: {
+    appDir: true,
+  },
+};
