5fc448c15e91beb60855b07d0d7af52df88da0f3
Maryam Nadeem <162018235+maryumxx@users.noreply.github.com>
2024-10-04 09:15:53 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-export default {
+const nextConfig = {
   experimental: {
     appDir: true,
   },
 };
 
+export default nextConfig;
